152a0b72601d5dde026ea7381ee65e59095642b2
yw314 <yw314@duke.edu>
2019-11-13 19:56:12 -0500

    Login dummy

---
+++ friends-app/src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col xs="12" sm="8" offset-sm="2" md="6" offset-md="3">
+        <v-card class="elevation-12">
+          <v-toolbar dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+            <v-toolbar-title>Login Form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                prepend-icon="email"
+                name="login"
+                label="Duke Email"
+                suffix="@duke.edu"
+                id="login"
+                type="text"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <!-- <v-spacer></v-spacer> -->
+            <v-btn block dark color="pink">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+    components: {
+    },
+})
+
+export default class Login extends Vue {
+  private username: string = '';
+  private password: string = '';
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- friends-app/src/views/Home.vue
+++ friends-app/src/views/Home.vue
@@ -1,21 +1,57 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <SignIn></SignIn>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col xs="12" sm="6" offset-sm="3" md="4" offset-md="4">
+          <v-img
+            :src="require('../assets/Friends.svg')"
+            contain
+            height="100"
+            width="400"
+          ></v-img>
+        </v-col>
+      </v-row>
+      <Login/>
+      <v-row no-gutters>
+        <v-col xs="12" sm="8" offset-sm="2" md="6" offset-md="3">
+          <v-img
+            :src="require('../assets/divider.png')"
+            height="20"
+          ></v-img>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-center" xs="12" sm="8" offset-sm="2" md="6" offset-md="3">
+          Don't have an account?
+          <v-btn block dark class="mt-3" color="green">
+            SIGN UP
+          </v-btn>
+        </v-col>
+      </v-row>
+      <!-- <div style="text-align: center" class="mt-3">
+        <p>Don't have an account?</p>
+      </div> -->
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-import SignIn from '@/components/SignIn.vue';
+import Login from '@/components/Login.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
-    SignIn,
+    Login,
   },
 };
 </script>
+
+<style scoped>
+.center {
+  text-align: center;
+  border: 3px solid green;
+}
+</style>
